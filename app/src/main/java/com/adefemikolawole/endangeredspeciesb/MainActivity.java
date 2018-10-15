30d5bb91225b7569f142f565775bbb8d2ef31686
Adefemi Kolawole <akolawole@ggc.edu>
2018-10-15 15:05:31 -0400

    changed action bar and status bar colors

--- app/src/main/java/com/adefemikolawole/endangeredspeciesb/MainActivity.java
+++ app/src/main/java/com/adefemikolawole/endangeredspeciesb/MainActivity.java
@@ -1,13 +1,29 @@
 package com.adefemikolawole.endangeredspeciesb;
 
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        changeStatusBarColor();
         setContentView(R.layout.activity_main);
+
     }
+
+    private void changeStatusBarColor() {
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.lightGreen)));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Window window = getWindow();
+            ((Window) window).addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.darkGreen));
+        }
+    }
+
 }
