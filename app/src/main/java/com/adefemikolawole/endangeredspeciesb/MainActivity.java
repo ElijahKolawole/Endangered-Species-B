3a8fa5b6b411ac0f60273cc1944d6ade3815f2dc
Adefemi Kolawole <akolawole@ggc.edu>
2018-10-15 16:36:18 -0400

    added a seperate   toolbar, added a snack bar method to display author. included the new toolbar in the MainActivity.java to use instead of the default toolbar

--- app/src/main/java/com/adefemikolawole/endangeredspeciesb/MainActivity.java
+++ app/src/main/java/com/adefemikolawole/endangeredspeciesb/MainActivity.java
@@ -1,20 +1,31 @@
 package com.adefemikolawole.endangeredspeciesb;
 
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+
+
 
 import static android.widget.AdapterView.*;
 
 public class MainActivity extends AppCompatActivity {
+
     final String TAG = MainActivity.class.getSimpleName().toString();
     Integer[] Animals = {R.drawable.eagle, R.drawable.elephant, R.drawable.gorilla, R.drawable.panda, R.drawable.panther, R.drawable.polar};
     ImageView pic;
@@ -24,7 +35,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Log.e(TAG, "Application begins here...");
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        changeStAcBArBgColor();
 
         GridView grid = findViewById(R.id.gridView);
         final ImageView pic = findViewById(R.id.imgLarge);
@@ -36,9 +51,12 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(getBaseContext(), "Selected Species " + (position + 1), Toast.LENGTH_LONG).show();
                 pic.setImageResource(Animals[position]);
+
             }
 
         });
+
+
 
     }
 
@@ -80,4 +98,20 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+    public void showAuthor(){
+        final LinearLayout coordinatorLayout = findViewById(R.id.linearLayout);
+        Snackbar snackbar = Snackbar.make(coordinatorLayout, "Author: Adefemi Kolawole \n ITEC 4550 - Fall 2018", Snackbar.LENGTH_LONG);
+        snackbar.show();
+
+    }
+
+    public void changeStAcBArBgColor(){
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.lightGreen)));
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Window window = getWindow();
+             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.darkGreen));
+        }
+    }
+
 }
